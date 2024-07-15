ffe720c054a6d1da5d6c1d05bf815576e61787e2
Robin Raju <robinraju@users.noreply.github.com>
2024-07-15 21:43:19 +0100

    Implement CRUD

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"html/template"
+	"github.com/robinraju/snippetbox/internal/models"
 	"net/http"
 	"strconv"
 )
@@ -10,22 +11,31 @@ import (
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
 
-	files := []string{
-		"./ui/html/base.go.html",
-		"./ui/html/pages/home.go.html",
-		"./ui/html/partials/nav.go.html",
-	}
-
-	ts, err := template.ParseFiles(files...)
-	if err != nil {
-		app.serverError(w, r, err)
-		return
-	}
-
-	err = ts.ExecuteTemplate(w, "base", nil)
+	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, r, err)
 	}
+
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%v\n", snippet)
+	}
+
+	//files := []string{
+	//	"./ui/html/base.go.html",
+	//	"./ui/html/pages/home.go.html",
+	//	"./ui/html/partials/nav.go.html",
+	//}
+	//
+	//ts, err := template.ParseFiles(files...)
+	//if err != nil {
+	//	app.serverError(w, r, err)
+	//	return
+	//}
+	//
+	//err = ts.ExecuteTemplate(w, "base", nil)
+	//if err != nil {
+	//	app.serverError(w, r, err)
+	//}
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
@@ -35,7 +45,16 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			http.NotFound(w, r)
+		} else {
+			app.serverError(w, r, err)
+		}
+	}
+
+	fmt.Fprintf(w, "%v", snippet)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
@@ -43,6 +62,14 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte("Save a new snippet..."))
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := 7
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, r, err)
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
--- docker/docker-entrypoint-initdb.d/init.sql
+++ docker/docker-entrypoint-initdb.d/init.sql
@@ -5,12 +5,13 @@ CREATE DATABASE snippetbox CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE snippetbox;
 
 -- Create a `snippets` table.
-CREATE TABLE snippets (
-                          id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                          title VARCHAR(100) NOT NULL,
-                          content TEXT NOT NULL,
-                          created DATETIME NOT NULL,
-                          expires DATETIME NOT NULL
+CREATE TABLE snippets
+(
+    id      INTEGER      NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title   VARCHAR(100) NOT NULL,
+    content TEXT         NOT NULL,
+    created DATETIME     NOT NULL,
+    expires DATETIME     NOT NULL
 );
 
 -- Add an index on the created column.
